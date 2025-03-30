bacf4317b67ffe67bf99cb8f410071d4a32897a2
00ffcc <yanqiwanrenhe@qq.com>
2025-03-30 23:55:47 +0800

    之前indexs搞太大了

--- dist/dist_test.py
+++ dist/dist_test.py
@@ -12,29 +12,43 @@ profile_kwargs = ProfileKwargs(
 accelerator = Accelerator(kwargs_handlers=[profile_kwargs])
 
 if accelerator.is_local_main_process:
-    embedding = SparseEmbedding(1000, 3, optimizer_params = {
+    embedding = SparseEmbedding(10000, 300, optimizer_params = {
                 "lr": 0.001,
                 "beta1": 0.9,
                 "beta2": 0.999,
                 "weight_decay": 0.0001,
                 "eps": 1e-8,
             })
-    indexs = torch.randint(0, 1000, (2, 200, 40000))
+    indexs = torch.randint(0, 10000, (2, 20, 4000), device=accelerator.device)
+# with torch.profiler.profile(
+#             activities=[
+#                 torch.profiler.ProfilerActivity.CPU,
+#                 torch.profiler.ProfilerActivity.CUDA],  # 分析 CPU 和 CUDA 活动
+#             schedule=torch.profiler.schedule(
+#                 wait=1,  # 前1步不采样
+#                 warmup=1,  # 第2步作为热身，不计入结果
+#                 active=3,  # 采集后面3步的性能数据
+#                 repeat=2),  # 重复2轮
+#             on_trace_ready=torch.profiler.tensorboard_trace_handler('./log'),  # 保存日志以供 TensorBoard 可视化
+#             record_shapes=True,  # 记录输入张量的形状
+#             profile_memory=True,  # 分析内存分配
+#             with_stack=True  # 记录操作的调用堆栈信息
+#         ) as prof:
 with accelerator.profile() as prof:
     for _ in range(10):
         if accelerator.is_local_main_process:
             embeds = embedding(indexs)
             # 按第0维切分
-            scatter_list = torch.split(embeds, 1, dim=0)
+            scatter_list = [embeds[0], embeds[1]]
         else:
             scatter_list = None
 
-        # out = torch.empty(3, device=accelerator.device)
-        # dist.scatter(out, scatter_list, src=0)
-        out = [None]
-        dist.scatter_object_list(out, scatter_list, src=0) # 太慢了
+        out = torch.empty((20, 4000, 300), device=accelerator.device)
+        dist.scatter(out, scatter_list, src=0)
+        # out = [None]
+        # dist.scatter_object_list(out, scatter_list, src=0) # 太慢了
+        # embed = out[0].to(accelerator.device)
 
-        embed = out[0].to(accelerator.device)
         prof.step()
         # profiler.export_chrome_trace('/NAS/wujunkang/guizhiyu/mole/dist/trace.json')  # 保存日志以供 Chrome Tracing 可视化
 
